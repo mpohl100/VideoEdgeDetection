7103d19065a4f41b439f61e5467de2321b96cafd
Michael Pohl <mpohl100@googlemail.com>
2020-03-22 21:34:19 +0100

    erster Entwurf der Gradientenfunktion

--- test1.cpp
+++ test1.cpp
@@ -25,6 +25,42 @@ Mat getContours(Mat const& img)
 	return contours;
 }
 
+Mat homeBrewEdgeDetection(Mat const& img)
+{
+	Mat ret(img);
+
+	Vec3b* retCenter;
+	const Vec3b* imgUpper;
+	const Vec3b* imgCenter;
+	const Vec3b* imgLower;
+	for (int j = 1; j < img.rows - 1; ++j)
+	{
+		retCenter = ret.ptr<Vec3b>(j);
+		imgUpper = img.ptr<Vec3b>(j - 1);
+		imgCenter = img.ptr<Vec3b>(j);
+		imgLower = img.ptr<Vec3b>(j+1);
+		for (int i = 1; i < img.cols - 1; ++i)
+		{
+			// 0=Blue, 1=Green, 2=Red
+			//for (int color = 0; color < 3; color++)
+			//{
+
+			//}
+			int color = 0;
+			float grad_tl_br = imgLower[i + 1][color] - imgUpper[i - 1][color];
+			float grad_cl_cr = imgCenter[i + 1][color] - imgCenter[i - 1][color];
+			float grad_bl_tr = imgUpper[i + 1][color] - imgLower[i - 1][color];
+			float grad_bc_tc = imgUpper[i][color] - imgLower[i][color];
+			float sqrt2 = std::sqrt(2.0);
+			float grad_x = grad_cl_cr + grad_tl_br * sqrt2 + grad_bl_tr * sqrt2;
+			float grad_y = -grad_bc_tc + grad_tl_br * sqrt2 - grad_bl_tr * sqrt2;
+			float grad_total = std::sqrt(grad_x * grad_x + grad_y * grad_y);
+			retCenter[i][color] = int(grad_total);
+		}
+	}
+	return ret;
+}
+
 int main(int argc, char** argv)
 {
 	VideoCapture cap("D:\ToiletBank.mp4"); //capture the video from web cam
@@ -69,7 +105,8 @@ int main(int argc, char** argv)
 			cout << "Cannot read a frame from video stream" << endl;
 			break;
 		}
-		Mat contours = getContours(imgOriginal);
+		//Mat contours = getContours(imgOriginal);
+		Mat contours = homeBrewEdgeDetection(imgOriginal);
 		imshow(threshold, contours); //show the thresholded image
 		imshow(original, imgOriginal); //show the original image
 
