c0671172dbf0a37a56b333fc2e849b3e08f9a46c
Michael Pohl <mpohl100@googlemail.com>
2020-03-25 21:50:43 +0100

    Revert "revert"
    This reverts commit 4ab98aecfb0e43fb061da20eaeba243963974a2e.

--- Detection.cpp
+++ Detection.cpp
@@ -2,13 +2,13 @@
 #include "Ant.h"
 #include <iostream>
 #include <opencv2\imgproc.hpp>
-#include "Detection.h"
 
 
 namespace od 
 {
-	std::tuple<int,int,int> gradient(int tl, int tc, int tr, int cl, int cr, int bl, int bc, int br)
+	int gradient(int tl, int tc, int tr, int cl, int cc, int cr, int bl, int bc, int br)
 	{
+		int grad = 0;
 		float grad_tl_br = br - tl;
 		float grad_cl_cr = cr - cl;
 		float grad_bl_tr = tr - bl;
@@ -17,28 +17,22 @@ namespace od
 		float grad_x = grad_cl_cr + grad_tl_br * sqrt2 + grad_bl_tr * sqrt2;
 		float grad_y = -grad_bc_tc + grad_tl_br * sqrt2 - grad_bl_tr * sqrt2;
 		float grad_total = std::sqrt(grad_x * grad_x + grad_y * grad_y);
+		grad = int(grad_total);
 		if (false) {
 			std::cout << "gradient of:\n";
 			std::cout << tl << " " << tc << " " << tl << '\n';
-			std::cout << cl << " " << "center" << " " << cl << '\n';
+			std::cout << cl << " " << cc << " " << cl << '\n';
 			std::cout << bl << " " << bc << " " << bl << '\n';
 			std::cout << "grad_x = " << grad_x << "; grad_y = " << grad_y << "\n";
-			std::cout << "grad = " << grad_total << '\n';
+			std::cout << "grad = " << grad << '\n';
 		}
-		return { int(grad_total), int(grad_x), int(grad_y),  };
+		return grad;
 	}
 
 	cv::Mat detect_edges(cv::Mat const& img)
 	{
-		Directions dir(img);
-		return dir.val;
-	}
-
-	Directions::Directions(cv::Mat const img)
-	{
-		cv::cvtColor(img, x_dir, cv::COLOR_BGR2GRAY);
-		cv::cvtColor(img, y_dir, cv::COLOR_BGR2GRAY);
-		cv::cvtColor(img, val, cv::COLOR_BGR2GRAY);
+		cv::Mat ret;
+		cv::cvtColor(img, ret, cv::COLOR_BGR2GRAY);
 		const cv::Vec3b* imgUpper;
 		const cv::Vec3b* imgCenter;
 		const cv::Vec3b* imgLower;
@@ -54,24 +48,17 @@ namespace od
 				for (int color = 0; color <= 2; color++)
 				{
 					int grad_c = gradient(imgUpper[j - 1][color], imgUpper[j][color], imgUpper[j + 1][color],
-						imgCenter[j - 1][color], imgCenter[j + 1][color],
+						imgCenter[j - 1][color], imgCenter[j][color], imgCenter[j + 1][color],
 						imgLower[j - 1][color], imgLower[j][color], imgLower[j + 1][color]);
 					max = grad_c > max ? grad_c : max;
 					sum += grad_c;
 				}
-				float value = float(max) / float(sum);
-				value = std::sqrt(value);
-				value *= max;
-				val.at<uchar>(i, j) = int(value);
+				float val = float(max) / float(sum);
+				val = std::sqrt(val);
+				val *= max;
+				ret.at<uchar>(i, j) = int(val);
 			}
 		}
-	}
-
-	DirPoint3D::DirPoint3D(float x, float y, float val)
-		: x_(int((x / 2) + 128) )
-		, y_(int((y / 2) + 128))
-		, val_(int(val))
-	{
-
+		return ret;
 	}
 }
--- Detection.h
+++ Detection.h
@@ -2,22 +2,12 @@
 
 #include "opencv2/core/mat.hpp"
 namespace od {
-
-	class DirPoint3D {
-	public:
-		DirPoint3D(float x, float y, float val);
-	private:
-		uchar x_;
-		uchar y_;
-		uchar val_;
-	};
-
 	/**
 	 * returns the gradient over a pixel
 	 * first calculate the x and y gradients over the pixel cc
 	 * then return the sqrt(grad_x**2 + grad_y**2)
 	 */
-	int gradient(int tl, int tc, int tr, int cl, int cr, int bl, int bc, int br);
+	int gradient(int tl, int tc, int tr, int cl, int cc, int cr, int bl, int bc, int br);
 	/**
 	 * returns a gray scale Mat, takes an BGR / RGB Mat
 	 * iterate over all pixels and calculate the color gradient over it
@@ -25,11 +15,4 @@ namespace od {
 	 * a white pixel means a super high descent and a black pixel means a planar surface
 	 */
 	cv::Mat detect_edges(cv::Mat const& img);
-
-	struct Directions {
-		cv::Mat x_dir;
-		cv::Mat y_dir;
-		cv::Mat val;
-		Directions(cv::Mat const img);
-	};
 }
