71c489ea4fe9c6fa78c16f50901cd187a3fc16c5
Michael Pohl <mpohl100@googlemail.com>
2022-05-08 18:26:00 +0200

    Added bars to see distribution of angles

--- VideoEdgeDetection.cpp
+++ VideoEdgeDetection.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <random>
@@ -62,10 +63,25 @@ Ball Ball::collide(cv::Mat contours, int x , int y)
 }
 
 struct Point { double x = 0; double y = 0; };
+struct Bar { double len = 0; };
+struct Result { Point point; Bar bar; };
 struct PolarVec { double len = 0; double angle = 0; };
 struct Data { PolarVec polarVec; Point point; };
-constexpr size_t N = 18;
-std::array<Point, N> calculate_orientation(cv::Mat const& gradient)
+constexpr size_t N = 360;
+
+template<class T, size_t N>
+size_t calc_index(double angle, std::array<T, N> const& arr)
+{
+	double factor = (angle + 180.0) / 360.0;
+	if (factor < 0)
+		factor = 0;
+	size_t index = size_t(factor * double(arr.size()));
+	if (index >= arr.size())
+		index = arr.size() - 1;
+	return index;
+}
+
+std::array<Result, N> calculate_orientation(cv::Mat const& gradient)
 {
 	std::vector<Data> directions;
 	directions.reserve(gradient.rows* gradient.cols);
@@ -73,7 +89,7 @@ std::array<Point, N> calculate_orientation(cv::Mat const& gradient)
 	std::array<double, N> sumLen;
 	for (auto& d : sumLen)
 		d = 0.0;
-	std::array<Point, N> partials = { {} };
+	std::array<Result, N> partials = { {} };
 	for (auto i = 0; i < gradient.rows; ++i)
 	{
 		currentRow = gradient.ptr<cv::Vec3b>(i);
@@ -84,13 +100,7 @@ std::array<Point, N> calculate_orientation(cv::Mat const& gradient)
 				continue;
 			double angle = currentRow[j][1] != 0 ? double(currentRow[j][1]) : double(-currentRow[j][2]);
 			directions.push_back({ { len, angle }, { double(i), double(j) } });
-			double factor = (angle + 180.0) / 360.0;
-			if (factor < 0)
-				factor = 0;
-			size_t index =  size_t(factor  * double(partials.size()));
-			if (index >= partials.size())
-				index = partials.size() - 1;
-			sumLen[index] += len;
+			sumLen[calc_index(angle, partials)] += len;
 		}
 	}
 	std::array<int, N> num;
@@ -98,14 +108,10 @@ std::array<Point, N> calculate_orientation(cv::Mat const& gradient)
 		i = 0;
 	for (const auto& dir : directions)
 	{
-		double factor = (dir.polarVec.angle + 180.0) / 360.0;
-		if (factor < 0)
-			factor = 0;
-		size_t index = size_t(factor * double(partials.size()));
-		if (index >= partials.size())
-			index = partials.size() - 1;
-		partials[index].x += dir.point.x * dir.polarVec.len/sumLen[index];
-		partials[index].y += dir.point.y * dir.polarVec.len / sumLen[index];
+		size_t index = calc_index(dir.polarVec.angle, partials);
+		partials[index].point.x += dir.point.x * dir.polarVec.len/ sumLen[index];
+		partials[index].point.y += dir.point.y * dir.polarVec.len / sumLen[index];
+		partials[index].bar.len = sumLen[index];
 		num[index]++;
 	}
 #if 0
@@ -117,6 +123,32 @@ std::array<Point, N> calculate_orientation(cv::Mat const& gradient)
 	}
 #endif
 	return partials;
+}
+
+template<size_t N>
+void draw_bars(cv::Mat& result, std::array<Result, N> partials)
+{
+	double sum = 0;
+	for (const auto& bar : partials)
+		sum += bar.bar.len;
+	for (auto& bar : partials)
+	{
+		bar.bar.len /= sum;
+		bar.bar.len *= 100.0; // in percent
+	}
+	size_t nth = 5;
+	auto partials_copy = partials;
+	auto nth_it = partials_copy.begin() + nth;
+	std::nth_element(partials_copy.begin(), nth_it,  partials_copy.end(), [](Result const& l, Result const& r) { return l.bar.len > r.bar.len; });
+	double max = partials_copy[nth].bar.len;
+	int barBreadth = int(double(result.rows) / double(N));
+	for (size_t i = 0; i < N; ++i)
+	{
+		cv::Point tl{ int(i) * barBreadth, 0 };
+		Bar& bar = partials[i].bar;
+		cv::Point br{ int(i + 1) * barBreadth, int((bar.len / max) * (double(result.cols) / 4)) };
+		cv::rectangle(result, cv::Rect(tl, br), cv::Scalar(0, 0, 256));
+	}
 }
 
 int main(int argc, char** argv)
@@ -145,9 +177,9 @@ int main(int argc, char** argv)
 		cv::Mat contours = od::detect_angles(imgOriginal);
 		cv::Mat gradient = od::detect_directions(imgOriginal);
 		auto partials = calculate_orientation(gradient);
-		size_t index = 0;
-		for (const auto& partial : partials)
-			cv::circle(contours, cv::Point(int(partial.x), int(partial.y)), 5, cv::Scalar(0, 0, 256));
+		//for (const auto& partial : partials)
+		//	cv::circle(contours, cv::Point(int(partial.point.x), int(partial.point.y)), 5, cv::Scalar(0, 0, 256));
+		draw_bars(contours, partials);
 		//if (j++ == 0)
 		//{
 		//	Ball ball(contours);
